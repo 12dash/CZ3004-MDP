635198455531e8af5837a4f1e545f3fb36b50016
12dash <soham.dandapath@gmail.com>
2021-02-05 09:05:18 +0800

    Commit Test

--- Algorithm/src/com/company/Main.java
+++ Algorithm/src/com/company/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String path = "example_4.txt";
+        String path = "example_1.txt";
         String[] p_string=  File_Read.read_file(path);
         int[][] obs = Map_Descriptor.get_map(p_string[0],p_string[1]);
 
         Arena arena = new Arena(20,15);
         arena.make_arena(obs);
         arena.get_view();
-            
+
     }
 }
