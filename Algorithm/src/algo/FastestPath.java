8cd37741963150da0b7fbe7d3aabd65a77a69498
12dash <soham.dandapath@gmail.com>
2021-02-16 12:53:25 +0800

    Communication added

--- Algorithm/src/algo/FastestPath.java
+++ Algorithm/src/algo/FastestPath.java
@@ -11,7 +11,6 @@ public class FastestPath {
         Grid wayPoint = arena.arena[wayPoint_cord[1]][wayPoint_cord[0]];
         search.start_search(arena, wayPoint, robot, false);
 
-
         search = new AStar();
         search.start_search(arena, arena.arena[1][13], robot, true);
         arena.display_solution(robot.path);
---
+++ Algorithm/src/communicationManager/commConstants.java
@@ -0,0 +1,8 @@
+package communicationManager;
+
+public class commConstants {
+
+    public static final String IP_ADDRESS = "10.27.229.182";
+    public static final int PORT = 8080;
+
+}
