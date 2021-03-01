725b6ef244e8065326d6e5f368fd9f8fab7daee0
Aditya Bansal <adityabansal10@gmail.com>
2021-03-01 23:04:31 +0800

    Cleans communication code

--- AlgorithmsNew/src/Communication/Communication.java
+++ AlgorithmsNew/src/Communication/Communication.java
@@ -3,7 +3,6 @@ package Communication;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 
 public class Communication extends Thread{
@@ -53,23 +52,13 @@ public class Communication extends Thread{
 
 
     public String recvMsg() {
-        String message;
+
         System.out.println("Receiving a message...");
 
         try {
-            System.out.println("Inside try block 0");
-//            StringBuilder sb = new StringBuilder();
             String input = reader.readLine();
-            System.out.println(input);
-            System.out.println("Inside try block 1");
+            System.out.println("Message Received: " + input);
             return input;
-
-//            if (input != null && input.length() > 0) {
-//                sb.append(input);
-//                System.out.println(sb.toString());
-//                message =  sb.toString();
-//                return message;
-//            }
         } catch (IOException e) {
             System.out.println("recvMsg() --> IOException");
         } catch (Exception e) {
@@ -79,30 +68,6 @@ public class Communication extends Thread{
 
         return null;
     }
-
-
-    public static void main(String[] args) {
-        Communication comm = new Communication();
-        comm.openConnection();
-
-        // TODO: Implement multi-threading
-        while (comm.isConnected()) {
-            comm.recvMsg();
-        }
-        while (true) {
-
-            Scanner inmMsg = new Scanner(System.in);
-            System.out.print("Recipient: ");
-            String rec = inmMsg.nextLine();
-            System.out.print("Message: ");
-            String msg = inmMsg.nextLine();
-            comm.sendMsg(rec, msg);
-        }
-        //comm.closeConnection();
-    }
-
-
-
 
     public void closeConnection() {
         System.out.println("Closing connection...");
@@ -140,112 +105,7 @@ public class Communication extends Thread{
         }
     }
 
-
     public boolean isConnected() {
         return conn.isConnected();
     }
 }
-
-
-/*
-
-
-    public void openConnection() {
-        System.out.println("Opening connection...");
-
-        try {
-            String HOST = "192.168.2.1";
-            int PORT = 8008;
-            conn = new Socket(HOST, PORT);
-
-            writer = new BufferedWriter(new OutputStreamWriter(new BufferedOutputStream(conn.getOutputStream())));
-            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-            System.out.println("openConnection() --> " + "Connection established successfully!");
-
-            return;
-        } catch (UnknownHostException e) {
-            System.out.println("openConnection() --> UnknownHostException");
-        } catch (IOException e) {
-            System.out.println("openConnection() --> IOException");
-        } catch (Exception e) {
-            System.out.println("openConnection() --> Exception");
-            System.out.println(e.toString());
-        }
-
-        System.out.println("Failed to establish connection!");
-    }
-
-    public void closeConnection() {
-        System.out.println("Closing connection...");
-
-        try {
-            reader.close();
-
-            if (conn != null) {
-                conn.close();
-                conn = null;
-            }
-            System.out.println("Connection closed!");
-        } catch (IOException e) {
-            System.out.println("closeConnection() --> IOException");
-        } catch (NullPointerException e) {
-            System.out.println("closeConnection() --> NullPointerException");
-        } catch (Exception e) {
-            System.out.println("closeConnection() --> Exception");
-            System.out.println(e.toString());
-        }
-    }
-
-    public void sendMsg(String msg, String msgType) {
-        System.out.println("Sending a message...");
-
-        try {
-            String outputMsg;
-            if (msg == null) {
-                outputMsg = msgType + "\n";
-            } else if (msgType.equals(MAP_STRINGS) || msgType.equals(BOT_POS)) {
-                outputMsg = msgType + " " + msg + "\n";
-            } else {
-                outputMsg = msgType + "\n" + msg + "\n";
-            }
-
-            System.out.println("Sending out message:\n" + outputMsg);
-            writer.write(outputMsg);
-            writer.flush();
-        } catch (IOException e) {
-            System.out.println("sendMsg() --> IOException");
-        } catch (Exception e) {
-            System.out.println("sendMsg() --> Exception");
-            System.out.println(e.toString());
-        }
-    }
-
-    public String recvMsg() {
-        System.out.println("Receiving a message...");
-
-        try {
-            StringBuilder sb = new StringBuilder();
-            String input = reader.readLine();
-
-            if (input != null && input.length() > 0) {
-                sb.append(input);
-                System.out.println(sb.toString());
-                return sb.toString();
-            }
-        } catch (IOException e) {
-            System.out.println("recvMsg() --> IOException");
-        } catch (Exception e) {
-            System.out.println("recvMsg() --> Exception");
-            System.out.println(e.toString());
-        }
-
-        return null;
-    }
-
-    public boolean isConnected() {
-        return conn.isConnected();
-    }
-}
-
- */
