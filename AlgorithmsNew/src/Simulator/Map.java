e622442c7352fbe67271d72585821d79d743a6c1
Aditya Bansal <adityabansal10@gmail.com>
2021-03-01 22:55:45 +0800

    big fix

--- AlgorithmsNew/src/Simulator/Map.java
+++ AlgorithmsNew/src/Simulator/Map.java
@@ -102,16 +102,18 @@ public class Map extends JPanel {
             }
         }
 
-        // Paint Fastest Path
-        for (Grid gr: robotReal.getPath()){
-            g.setColor(SimulatorConstants.C_PATH);
-            g.fillRect(_gridCells[gr.getY()][gr.getX()].cellX + SimulatorConstants.MAP_X_OFFSET, _gridCells[gr.getY()][gr.getX()].cellY, _gridCells[gr.getY()][gr.getX()].cellSize, _gridCells[gr.getY()][gr.getX()].cellSize);
-        }
+        if(!simulate) {
+            // Paint Fastest Path
+            for (Grid gr : robotReal.getPath()) {
+                g.setColor(SimulatorConstants.C_PATH);
+                g.fillRect(_gridCells[gr.getY()][gr.getX()].cellX + SimulatorConstants.MAP_X_OFFSET, _gridCells[gr.getY()][gr.getX()].cellY, _gridCells[gr.getY()][gr.getX()].cellSize, _gridCells[gr.getY()][gr.getX()].cellSize);
+            }
 
-        // Paint WayPoint
-        if(!this.simulate){
-            g.setColor(SimulatorConstants.C_WAYPOINT);
-            g.fillRect(_gridCells[waypoint_y][waypoint_x].cellX + SimulatorConstants.MAP_X_OFFSET, _gridCells[waypoint_y][waypoint_x].cellY, _gridCells[waypoint_y][waypoint_x].cellSize, _gridCells[waypoint_y][waypoint_x].cellSize);
+            // Paint WayPoint
+            if (!this.simulate) {
+                g.setColor(SimulatorConstants.C_WAYPOINT);
+                g.fillRect(_gridCells[waypoint_y][waypoint_x].cellX + SimulatorConstants.MAP_X_OFFSET, _gridCells[waypoint_y][waypoint_x].cellY, _gridCells[waypoint_y][waypoint_x].cellSize, _gridCells[waypoint_y][waypoint_x].cellSize);
+            }
         }
 
         // Paint the robot on-screen.
