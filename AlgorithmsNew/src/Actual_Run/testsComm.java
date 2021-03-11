3833288ce703442fbeebc51c0bdf8d297a43d333
12dash <soham.dandapath@gmail.com>
2021-03-11 17:05:14 +0800

    Updates

--- AlgorithmsNew/src/Actual_Run/testsComm.java
+++ AlgorithmsNew/src/Actual_Run/testsComm.java
@@ -12,7 +12,7 @@ public class testsComm {
 
     public static void main(String[] args){
         Communication comm = Communication.getCommunication();
-        String[] p1p2 = new String[]{"FFFFFFFFFFFFFFFFFFF91FF001E003C007800F001E007C00F801F003E003C00F803F007E00FF","0700000000000001C00044444400000400002000"};
+        String[] p1p2 = new String[]{"FFFFFFFFFFFFFFFFFFF91FF001E003C007800F001E007C00F801F003E003C00F803F007E00FF","000700000000000001C000444444000004000020"};
         String anMssg = "{p1:" + p1p2[0] + ",p2:" + p1p2[1] +"}";
         comm.openConnection();
         Scanner sc= new Scanner(System.in);
